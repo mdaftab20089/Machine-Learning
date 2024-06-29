020bd5b18956de793683a2aec305e73a638f2adf
mdaftab20089 <aftabrahi20089@gmail.com>
2024-06-29 12:13:04 +0530

    add

---
+++ Packages/__init__.py
@@ -0,0 +1,1 @@
+'''this is a empty file for creating our own packages and modules'''
--- Packages/addition.py
+++ Packages/addition.py
@@ -1,9 +1,12 @@
 def sum(a,b):
     return a+b
 
+print(sum(3,4))
 
 def diff(a,b):
     if a>b:
         return a-b
     else:
         return b-a
+    
+print(diff(4,5))   
